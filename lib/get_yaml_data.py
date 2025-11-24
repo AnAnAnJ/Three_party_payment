994ad66e1a14911765b179718907ad9678b45801
Jill <13555730249@163.com>
2025-11-24 15:21:06 +0800

    解决文件读取问题

--- lib/get_yaml_data.py
+++ lib/get_yaml_data.py
@@ -1,35 +1,32 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import allure
 import yaml
-import pathlib
+
 
 def read_data_file(filename):
     with open(filename, 'r',encoding="utf-8") as file:
-        print(f'执行读取文件信息，文件地址为：{filename}')
+        # print(f'执行读取文件信息，文件地址为：{filename}')
         return [line.strip() for line in file.readlines()]
 
 def read_yaml_file(filename):
     yaml_file = yaml.safe_load(open(filename,encoding="utf-8"))
-    print(f'执行读取文件信息，文件地址为：{filename}')
+    # print(f'执行读取文件信息，文件地址为：{filename}')
     return yaml_file
 
-
-
-
-def get_yaml_datas(filename, data, group, case):
-    # 获取当前脚本所在目录
-    root_path = pathlib.Path(__file__).parent.absolute()
-    # 正确构建目标文件路径
-    yaml_path = (root_path / '..' / filename).resolve()
-    # if not yaml_path.exists():
-    #     raise FileNotFoundError(f"YAML file not found at: {yaml_path}")
-    yaml_data = read_yaml_file(str(yaml_path))
-    values = yaml_data.get(data).get(group).get(case)
-    print(values)
+#获取yaml文件的路径，并读取测试用例数据
+def get_yaml_datas(filename,data,case):
+    #读取当前路径
+    root_path = os.path.dirname(os.path.abspath(__file__))
+    #读取yaml文件路径
+    yaml_path = os.sep.join([root_path,'..',filename])
+    yaml_data = read_yaml_file(yaml_path)
+    values = yaml_data.get(data).get(case).get('staging')
+    # ids = yaml_data.get(data).get(case).get('ids')
     return values
 
 
 if __name__ == '__main__':
-    # Example usage
-    print(get_yaml_datas("./func/url_config.yaml", "url_config", "Viva", "staging"))
+    print(get_yaml_datas("func/url_config.yaml","url","Viva"))
+
